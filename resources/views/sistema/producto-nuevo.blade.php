2c05045bd404687c7563814c2a22efe7f842c876
lucasEB22 <lucas.benitez2291@gmail.com>
2023-12-12 20:44:55 -0300

    producto-nuevo

--- resources/views/sistema/producto-nuevo.blade.php
+++ resources/views/sistema/producto-nuevo.blade.php
@@ -5,7 +5,7 @@
       globalId = '';
       <?php $globalId = ""; ?>
 </script>
-
+<script src="https://cdn.ckeditor.com/ckeditor5/40.1.0/classic/ckeditor.js"></script>
 @endsection
 @section('breadcrumb')
 <ol class="breadcrumb">
