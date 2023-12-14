0efe6bb33d69d64f1490cf5b9cc47311fe44e0bd
lucasEB22 <lucas.benitez2291@gmail.com>
2023-12-14 20:26:52 -0300

    categoria

--- app/Http/Controllers/ControladorCategoria.php
+++ app/Http/Controllers/ControladorCategoria.php
@@ -1,29 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Entidades\Sistema\Producto;
+use App\Entidades\Sistema\Categoria;
 require app_path() . '/start/constants.php';
 
-class ControladorProducto extends Controller
+class ControladorCategoria extends COntroller
 {
       public function nuevo()
       {
-            $titulo = "Nuevo producto";
-            return view('sistema.producto-nuevo', compact('titulo'));
+            $titulo = "Nueva categoría";
+            return view('sistema.categoria-nuevo', compact('titulo'));
       }
 
       public function index()
       {
-            $titulo = "Listado de productos";
-            return view('sistema.producto-listar', compact('titulo'));
+            $titulo = "Listado de categorías";
+            return view('sistema.categoria-listar', compact('titulo'));
       }
 
       public function guardar(Request $request){
             try {
                //Define la entidad servicio
                $titulo = "Modificar cliente";
-               $entidad = new Producto();
+               $entidad = new Categoria();
                $entidad->cargarDesdeRequest($request);
    
                //validaciones
@@ -31,9 +32,9 @@ class ControladorProducto extends Controller
                    $msg["ESTADO"] = MSG_ERROR;
                    $msg["MSG"] = "Complete todos los datos";
    
-                   $producto = new Producto();
-                   $producto->obtenerPorId($entidad->idproducto);
-                   return view('sistema.producto-nuevo', compact('msg', 'producto', 'titulo')) . '?id=' . $entidad->idproducto;
+                   $categoria = new Categoria();
+                   $categoria->obtenerPorId($entidad->idcategoria);
+                   return view('sistema.categoria-nuevo', compact('msg', 'categoria', 'titulo')) . '?id=' . $entidad->idcategoria;
    
                } else {
                    if ($_POST["id"] > 0) {
@@ -50,9 +51,9 @@ class ControladorProducto extends Controller
                        $msg["MSG"] = OKINSERT;
                    }
                 
-                   $_POST["id"] = $entidad->idproducto;
-                   $titulo = "Listado de productos";
-                   return view('sistema.producto-listar', compact('titulo', 'msg'));
+                   $_POST["id"] = $entidad->idcategoria;
+                   $titulo = "Listado de categorias";
+                   return view('sistema.categoria-listar', compact('titulo', 'msg'));
                }
            } catch (Exception $e) {
                $msg["ESTADO"] = MSG_ERROR;
