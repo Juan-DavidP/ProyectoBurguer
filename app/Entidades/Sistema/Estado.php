b655384eec4b6b003b24b4a015d3f50983874f9d
Juan-DavidP <juandavidpelaezvilla@gmail.com>
2023-12-26 16:53:50 -0500

    commit

--- app/Entidades/Sistema/Estado.php
+++ app/Entidades/Sistema/Estado.php
@@ -5,7 +5,7 @@ namespace App\Entidades\Sistema;
 use DB;
 use Illuminate\Database\Eloquent\Model;
 
-class EstadoSucursal extends Model
+class Estado extends Model
 {
       protected $table = 'estados';
       public $timestamps = false;
