9d23e39fbc556670a900063f8f47b53eb5b767e6
Juan-DavidP <juandavidpelaezvilla@gmail.com>
2024-01-02 18:19:02 -0500

    clientes

--- app/Http/Controllers/ControladorWebRegistro.php
+++ app/Http/Controllers/ControladorWebRegistro.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Entidades\Sistema\Cliente;
 use Illuminate\Http\Request;
 use Session;
+use DB;
 
 class ControladorWebRegistro extends Controller
 {
@@ -12,13 +14,34 @@ class ControladorWebRegistro extends Controller
             return view("web.registro");
       }
 
-      public function registrar(Request $request)
-      {
-             print_r( $request->input('txtNombre'));
-             print_r( $request->input('txtApellido'));
-             print_r( $request->input('txtCorreo'));
-             print_r( $request->input('txtClave'));
-             print_r( $request->input('txtTelefono'));
-             print_r( $request->input('txtDni'));
-      }
+      
+    public function registrar(Request $request)
+    {
+        $cliente = new Cliente();
+
+        $nombre = $request->input('txtNombre');
+        $apellido = $request->input('txtApellido');
+        $correo = $request->input('txtCorreo');
+        $clave = $request->input('txtClave');
+        $telefono = $request->input('txtTelefono');
+        $dni = $request->input('txtDni');
+
+        $sql = "INSERT INTO clientes (
+                  nombre,
+                  apellido,
+                  correo,
+                  telefono,
+                  dni,
+                  clave
+            ) VALUES (?, ?, ?, ?, ?, ?);";
+        $result = DB::insert($sql, [
+            $nombre,
+            $apellido,
+            $correo,
+            $telefono,
+            $dni,
+            $clave,
+        ]);
+        // return $this->idcliente = DB::getPdo()->lastInsertId();
+    }
 }
