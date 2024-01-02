0ae88dfaf67aa6b53f206a8b7aa9161eadb46e2e
ntarche <nelson.tarche@gmail.com>
2024-01-02 20:28:24 -0300

    login

--- app/Http/Controllers/ControladorWebRegistro.php
+++ app/Http/Controllers/ControladorWebRegistro.php
@@ -18,30 +18,15 @@ class ControladorWebRegistro extends Controller
     public function registrar(Request $request)
     {
         $cliente = new Cliente();
-
-        $nombre = $request->input('txtNombre');
-        $apellido = $request->input('txtApellido');
-        $correo = $request->input('txtCorreo');
-        $clave = $request->input('txtClave');
-        $telefono = $request->input('txtTelefono');
-        $dni = $request->input('txtDni');
-
-        $sql = "INSERT INTO clientes (
-                  nombre,
-                  apellido,
-                  correo,
-                  telefono,
-                  dni,
-                  clave
-            ) VALUES (?, ?, ?, ?, ?, ?);";
-        $result = DB::insert($sql, [
-            $nombre,
-            $apellido,
-            $correo,
-            $telefono,
-            $dni,
-            $clave,
-        ]);
-        // return $this->idcliente = DB::getPdo()->lastInsertId();
+        $cliente->nombre = $request->input('txtNombre');
+        $cliente->apellido = $request->input('txtApellido');
+        $cliente->correo = $request->input('txtCorreo');
+        $cliente->clave = $request->input('txtClave');
+        $cliente->telefono = $request->input('txtTelefono');
+        $cliente->dni = $request->input('txtDni');
+        $cliente->insertar();
+        if($cliente->idcliente != ""){
+            return redirect("/login");
+        }
     }
 }
